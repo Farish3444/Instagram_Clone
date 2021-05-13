6e7582f96541f12b6d3352299a283259a035b95b
APPLE <apple@APPLEs-MacBook-Pro.local>
2021-05-13 20:31:33 +0530

    ReuseComp

---
+++ src/Component/Story/ReAvatar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+}));
+
+export default function ReAvatar({src}) {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Avatar alt="Femy Sharp" 
+      src={src} 
+    //   style={style}    
+      />
+      {/* <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" /> */}
+    </div>
+  );
+}
