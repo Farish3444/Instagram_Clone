a093473f2d53789c97d1bb42eb4dee8e267a3f07
APPLE <apple@APPLEs-MacBook-Pro.local>
2021-05-14 17:05:02 +0530

    .

--- src/App.js
+++ src/App.js
@@ -3,20 +3,44 @@ import './App.css';
 import HeaderPart from './Header/HeadPart';
 import Story from './Component/Story/Story';
 import NewsFeed from './Component/Story/NewsFeed/NewsFeed';
+import Login from './Login/Login';
+// 
+import {
+  Route,
+  Switch,
+  Link,
+  Redirect,
+  BrowserRouter
+  } 
+from 'react-router-dom';
+
+const main = (
+<Route component={App}>
+      <Route path='/HeaderPart' component={HeaderPart} />
+      <Route path="/Story" component={Story}/>
+      <Route path='/NewsFeed' component={NewsFeed}/>
+</Route>
 
 
+)
 
 function App() {
   return (
     <div className="App">
     <React.Fragment>
-      <HeaderPart/>
-      <Story />
-      <NewsFeed />
+    
+    {main}
+  
+      {/* <HeaderPart/> */}
+      {/* <Story /> */}
+      {/* <NewsFeed /> */}
+      <Login />
     </React.Fragment>
     </div>
   );
 }
+
+
 
 
 
